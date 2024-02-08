f4258ac91ea0953b10b9fcc8878d3f7200fdc70f
Maulana Aghni <tubagus.maulana@runsystem.id>
2024-02-08 22:22:31 +0700

    add dictionary

---
+++ 0_basic/dictionary.py
@@ -0,0 +1,14 @@
+person1 = {
+    'name' : 'John',
+    'occupation': 'Software Engineer',
+    'marital_status' : False,
+    'friends' : ['Joey', 'Ross', 'Chandler', 'Monica', 'Rachel', 'Phoebe']
+}
+
+# call Dictionary property
+print(person1['name'])
+print(person1['occupation'])
+print(person1['marital_status'])
+print(person1['friends'])
+# access list in dictionary
+print(person1['friends'][1])
